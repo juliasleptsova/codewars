22a1ace62c8c769e824c89428aedbd85e082dd21
julimorozova <yuli.sleptsova@gmail.com>
2021-07-13 15:48:24 +0300

    add a solution for the task about palindrome

---
+++ src/string/palindrome.js
@@ -0,0 +1,13 @@
+function isPalindrome(line) {
+    let str = String(line);
+    for (let i = 0; i < Math.floor(str.length /2 ); i++) {
+
+        if (str[i] !== str[str.length - i - 1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
